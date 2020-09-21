95e02f677d079d0401d50c9839846b6a5c289562
rhday <54642787+rhday@users.noreply.github.com>
2020-09-21 13:24:02 +0100

    migrated and tested my migrations

--- db/migrate/20200921111814_create_comments.rb
+++ db/migrate/20200921111814_create_comments.rb
@@ -2,11 +2,8 @@ class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
       t.string :content
-      t.resources :user
-      t.resources :post
-
-      t.belongs_to :user, foreign_key: true
-      t.belongs_to :post, foreign_key: true
+      t.references :user, foreign_key: true
+      t.references :post, foreign_key: true
 
       t.timestamps
     end
