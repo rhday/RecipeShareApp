743996198a624694206ca5fe27425b19b23212b7
rhday <54642787+rhday@users.noreply.github.com>
2020-09-21 16:28:22 +0100

    build out user show route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,24 @@ class UsersController < ApplicationController
     end 
 
     def create
+        @user = User.new(user_params)
+        if @user.save
+            #log them in
+            session[:user_id] = @user.id
+            #redirect to the show page 
+            redirect_to @user
+        else
+            render :new
+        end
     end
 
     def show
     end 
 
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end
 
 end
