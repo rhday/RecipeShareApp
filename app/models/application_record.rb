95e4a6d8f4de7281f61f87ab70d238fe62452ca4
rhday <54642787+rhday@users.noreply.github.com>
2020-09-22 14:39:05 +0100

    built navbar

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,20 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  #gives access to these helper methods in the views
+  helper_method :current_user, :logged_in?
+
+  private 
+
+  def current_user
+    @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!session[:user_id]
+  end 
+
+  def redirect_if_not_logged_in
+    redirect_to '/' if !logged_in?
+  end 
 end
