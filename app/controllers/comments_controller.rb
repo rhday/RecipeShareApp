22c4bb4044b5105f14c0e1a679dc97576129b7ae
rhday <54642787+rhday@users.noreply.github.com>
2020-10-01 13:14:28 +0100

    built strong params for comment controller, moving on to create

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,13 @@
 class CommentsController < ApplicationController
 
+    before_action :redirect_if_not_logged_in
+
     def new
+        @comment = Comment.new
     end 
 
-    def create 
+    def create
+        @comment = 
     end 
 
     def index
@@ -15,6 +19,9 @@ class CommentsController < ApplicationController
     private
 
     def comment_params
+        params.require(:comment).permit(:content, :user_id, :post_id)
     end 
-    
+
 end
+
+
